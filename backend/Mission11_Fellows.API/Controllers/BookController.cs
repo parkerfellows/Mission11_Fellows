19f8ee3d4b569cfd3706b5d08d7c02ea3e7e2420
parkerfellows <fellowsparker@gmail.com>
2025-03-17 19:35:16 -0600

    Phase 1

---
+++ backend/Mission11_Fellows.API/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Fellows.API.Data;
+
+namespace Mission11_Fellows.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookStoreDbContext _context;
+
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> GetBooks()
+        {
+            var books = _context.Books.ToList();
+            return books;
+        }
+    }
+}
---
+++ backend/Mission11_Fellows.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission11_Fellows.API.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
