8b9ccbf9e431f5d500fd242b4e80f080fa717917
parkerfellows <fellowsparker@gmail.com>
2025-03-21 18:54:22 -0600

    finished code

--- frontend/src/BookList.tsx
+++ frontend/src/BookList.tsx
@@ -1,36 +1,105 @@
 import { useState, useEffect } from 'react';
 import { Book } from './types/Book';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function BookList() {
   const [books, setBooks] = useState<Book[]>([]);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [pageNum, setPageNum] = useState<number>(1);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const response = await fetch('https://localhost:5000/api/Book');
+      const response = await fetch(
+        `https://localhost:5000/api/Book?pageSize=${pageSize}&pageNum=${pageNum}`
+      );
       const data = await response.json();
-      setBooks(data);
+      setBooks(data.books); // <-- Check This If Getting Errors
+      setTotalItems(data.totalNumBooks);
+      setTotalPages(Math.ceil(data.totalNumBooks / pageSize));
     };
     fetchBooks();
-  }, []);
+  }, [pageSize, pageNum]);
 
   return (
     <>
       <h1>Book List</h1>
       <br />
       {books.map((b) => (
-        <div id="bookCard">
-          <h3>{b.title}</h3>
-          <ul>
-            <li>Author: {b.author}</li>
-            <li>Publisher: {b.publisher}</li>
-            <li>ISBN: {b.isbn}</li>
-            <li>Classification: {b.classification}</li>
-            <li>Category: {b.category}</li>
-            <li>Page Count: {b.pageCount}</li>
-            <li>Price: ${b.price}</li>
-          </ul>
+        <div id="bookCard" className="card" key={b.isbn}>
+          <h3 className="card-title">{b.title}</h3>
+          <div className="card-body">
+            <ul className="list-unstyled">
+              <li>
+                <strong>Author: </strong>
+                {b.author}
+              </li>
+              <li>
+                <strong>Publisher: </strong>
+                {b.publisher}
+              </li>
+              <li>
+                <strong>ISBN: </strong>
+                {b.isbn}
+              </li>
+              <li>
+                <strong>Classification: </strong>
+                {b.classification}
+              </li>
+              <li>
+                <strong>Category: </strong>
+                {b.category}
+              </li>
+              <li>
+                <strong>Page Count: </strong>
+                {b.pageCount}
+              </li>
+              <li>
+                <strong>Price: </strong>${b.price}
+              </li>
+            </ul>
+          </div>
         </div>
       ))}
+
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
+
+      {[...Array(totalPages)].map((_, i) => (
+        <button
+          key={i + 1}
+          onClick={() => setPageNum(i + 1)}
+          disabled={pageNum === i + 1}
+        >
+          {i + 1}
+        </button>
+      ))}
+
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
+
+      <br />
+      <label>
+        Results per page:
+        <select
+          value={pageSize}
+          onChange={(p) => {
+            setPageSize(Number(p.target.value));
+            setPageNum(1);
+          }}
+        >
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={20}>20</option>
+        </select>
+      </label>
     </>
   );
 }
